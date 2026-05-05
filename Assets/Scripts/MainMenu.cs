92f9118a6068ce682875076fd14152b113f50d6a
LuciaAndres <167798945+LuciaAndres@users.noreply.github.com>
2026-05-05 20:29:36 +0200

    intento de munu inicial

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    void Jugar()
+    {
+        
+    }
+    void Salir()
+    {
+        Debug.Log("Saliendo...");
+        Application.Quit();
+    }
+}
